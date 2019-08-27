4c858ae6dd22882f51d2c0d5f1d9396225ef9c86
Gustavo Cosenza <gucosenza@gmail.com>
2019-08-27 17:47:41 -0300

    commit inicial

---
+++ Orama/Orama/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Orama
+//
+//  Created by gustavo.cosenza on 26/08/19.
+//  Copyright © 2019 gustavo.cosenza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Orama/OramaTests/OramaTests.swift
@@ -0,0 +1,34 @@
+//
+//  OramaTests.swift
+//  OramaTests
+//
+//  Created by gustavo.cosenza on 26/08/19.
+//  Copyright © 2019 gustavo.cosenza. All rights reserved.
+//
+
+import XCTest
+@testable import Orama
+
+class OramaTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
